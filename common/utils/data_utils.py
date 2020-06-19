1a6325145796ed57c512926a0c9df8cc5619dff0
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2020-06-19 15:30:03 +0100

    fix: join path with python util instead of +

--- common/utils/data_utils.py
+++ common/utils/data_utils.py
@@ -1,5 +1,6 @@
 from numpy import savetxt, loadtxt
 from torch import save
+import os
 
 
 class DataUtils:
@@ -14,8 +15,8 @@ class DataUtils:
         :param data_path: The data path where to save the labels.
         :param data_set: The dataset to save.
         """
-        save(data_set.data.unsqueeze(1), data_path + "_test.pth")
-        save(data_set.targets, data_path + "_test_labels.pth")
+        save(data_set.data.unsqueeze(1), os.path.join(data_path, "_test.pth"))
+        save(data_set.targets, os.path.join(data_path, "_test_labels.pth"))
 
     @staticmethod
     def save_indices(train_indices, test_indices, data_path):
@@ -25,8 +26,8 @@ class DataUtils:
         :param test_indices: The test indices to save.
         :param data_path: The data path where the indices should be stored.
         """
-        savetxt(data_path + 'train_indices.csv', train_indices, delimiter=',', fmt='%i')
-        savetxt(data_path + 'test_indices.csv', test_indices, delimiter=',', fmt='%i')
+        savetxt(os.path.join(data_path, 'train_indices.csv'), train_indices, delimiter=',', fmt='%i')
+        savetxt(os.path.join(data_path, 'test_indices.csv'), test_indices, delimiter=',', fmt='%i')
 
     @staticmethod
     def load_indices(data_path):
@@ -35,6 +36,6 @@ class DataUtils:
         :param data_path: The data path where the indices are stored.
         :return: The loaded train and test indices.
         """
-        train_indices = loadtxt(data_path + 'train_indices.csv', delimiter=',')
-        test_indices = loadtxt(data_path + 'test_indices.csv', delimiter=',')
+        train_indices = loadtxt(os.path.join(data_path, 'train_indices.csv'), delimiter=',')
+        test_indices = loadtxt(os.path.join(data_path, 'test_indices.csv'), delimiter=',')
         return train_indices.astype(int), test_indices.astype(int)
