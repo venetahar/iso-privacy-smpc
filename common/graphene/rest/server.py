4143f36224a26baf99a52b32b84c943dcbcb7c1b
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2020-09-30 23:23:27 +0100

    add: initial version of Flask REST API for inference

---
+++ common/graphene/rest/server.py
@@ -0,0 +1,124 @@
+import torch
+from flask import Flask, request, abort, Response, json
+from werkzeug.utils import secure_filename
+
+import sys
+sys.path.append("../..")
+
+# required to successfully load saved model
+from malaria.common.conv_pool_model import ConvPoolModel
+from mnist.common.conv_model import ConvModel
+from mnist.common.fully_connected_model import FullyConnectedModel
+
+# preprocess input images
+from malaria.common.malaria_data_loader import malaria_transform
+from mnist.common.mnist_data_loader import mnist_transform
+
+app = Flask(__name__)
+
+WORKDIR = 'rest/workdir'
+
+models = {
+    "mnist_conv_model": "mnist/models/alice_conv1_model.pth",
+    "mnist_fc_model": "mnist/models/alice_fc3_model.pth",
+    "malaria_conv_model": "malaria/models/alice_convpool_model.pth"
+}
+
+transforms = {
+    "mnist": mnist_transform,
+    "malaria": malaria_transform
+}
+
+
+def save_file_received(file_obj):
+    filename = secure_filename(file_obj.filename)
+    file_obj.save(filename)
+
+
+def load_model(model_filename):
+    from common.utils.sys_utils import cd
+
+    with cd(WORKDIR):
+        return torch.load(model_filename)
+
+
+def load_new_model(model_key):
+
+    global model, selected_model
+
+    if selected_model != model_key:
+        model_filename = models[model_key]
+        model = load_model(model_filename)
+        selected_model = model_key
+    return model, selected_model
+
+
+def load_image(image_file, model_type):
+    from PIL import Image
+
+    img = Image.open(image_file)
+    transform = transforms[model_type]()
+
+    timg = transform(img)
+    timg.unsqueeze_(0)
+
+    return timg
+
+
+def test(in_model, data):
+    in_model.eval()
+    with torch.no_grad():
+        outputs = in_model(data)
+        _, y_hat = outputs.max(1)
+        prediction = y_hat.item()
+
+        return prediction
+
+
+@app.route('/api/predict', methods=['POST'])
+def predict():
+    global model, selected_model
+
+    # handle input data file
+    if 'input_file' not in request.files:
+        print('No file part in request!')
+        abort(500)
+    file = request.files['input_file']
+
+    if file.filename == '':
+        print("No file uploaded!")
+        abort(500)
+
+    save_file_received(file_obj=file)
+
+    # handle model selection parameter
+    if 'model' not in request.form:
+        print('No model part in request!')
+        abort(500)
+    model_key = request.form["model"]
+
+    if model_key not in models:
+        print('Unknown model requested!')
+        abort(500)
+
+    # perform prediction
+    model_type = model_key.split('_')[0]
+    input_image = load_image(file, model_type)
+
+    model, selected_model = load_new_model(model_key)
+    prediction = test(model, input_image)
+
+    response = Response(response=json.dumps(prediction),
+                        status=200,
+                        mimetype='application/json')
+
+    return response
+
+
+# Place server initialisation code here
+selected_model = "mnist_fc_model"
+model = load_model(models[selected_model])
+
+
+if __name__ == '__main__':
+    app.run()
